29b75faf6297b676a03189e5dbfdfa87b055feb5
birukSebsibe <bksebsibe55@gmail.com>
2021-09-17 02:29:29 -0400

    BARACK COMMIT ON  PROJECT STRUCTURE

---
+++ loginPhp/page/contact.php
@@ -0,0 +1,64 @@
+<?php
+
+session_start();
+?>
+<?php
+include "include/header.php";
+
+
+?>
+
+
+<!DOCTYPE html>
+    <html>
+		<head>
+        <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/st.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+<style>
+
+.hero {
+    position: center;
+    left: 690px;
+    top: 430px; 
+}
+
+.my-center{
+        margin: 0 auto;
+        width: 300px;
+      }
+
+    </style>
+        </head>
+        <body>  
+            <section> 
+                <div class="my-center">
+                    <div class="contact-form">
+
+                    
+                        <p><b>Contact Us</b></p><br>
+
+                        <div>
+                        <span class="form-info">Address : Bole Parklane Tower, Addis Ababa Ethiopia</span><br>
+                        <span class="form-info">Phone no : +251900000000</span><br>
+                        <span class="form-info">Email : AITTech@gmail.com</span><br>
+                        </div>
+                        
+                    
+                        
+                    </div>          
+                    
+        </div>
+    </section>
+    
+        
+        </body>
+        
+        </html>
+    
+       
